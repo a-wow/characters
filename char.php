fb05d910494f80c90855602bd7fb670d1242e8f6
Null <107396951+a-wow@users.noreply.github.com>
2024-10-23 15:47:33 +0300

    Create char.php

---
+++ char.php
@@ -0,0 +1,73 @@
+<?php
+$class_image = array(
+    1 => '/images/classes/1.png',
+    2 => '/images/classes/2.png',
+    3 => '/images/classes/3.png',
+    4 => '/images/classes/4.png',
+    5 => '/images/classes/5.png',
+    6 => '/images/classes/6.png',
+    7 => '/images/classes/7.png',
+    8 => '/images/classes/8.png',
+    9 => '/images/classes/9.png',
+    11 => '/images/classes/11.png'
+);
+
+$race_image = array(
+    '1' => array(
+        '0' => '/images/race/1-0.png', '1' => '/images/race/1-1.png'
+    ),
+    '2' => array(
+        '0' => '/images/race/2-0.png', '1' => '/images/race/2-1.png'
+    ),
+    '3' => array(
+        '0' => '/images/race/3-0.png', '1' => '/images/race/3-1.png'
+    ),
+    '4' => array(
+        '0' => '/images/race/4-0.png', '1' => '/images/race/4-1.png'
+    ),
+    '5' => array(
+        '0' => '/images/race/5-0.png', '1' => '/images/race/5-1.png'
+    ),
+    '6' => array(
+        '0' => '/images/race/6-0.png', '1' => '/images/race/6-1.png'
+    ),
+    '7' => array(
+        '0' => '/images/race/7-0.png', '1' => '/images/race/7-1.png'
+    ),
+    '8' => array(
+        '0' => '/images/race/8-0.png', '1' => '/images/race/8-1.png'
+    ),
+    '10' => array(
+        '0' => '/images/race/10-0.png', '1' => '/images/race/10-1.png'
+    ),
+    '11' => array(
+        '0' => '/images/race/11-0.png', '1' => '/images/race/11-1.png'
+    )
+);
+
+$class_names = [
+    1 => 'Воин',
+    2 => 'Паладин',
+    3 => 'Охотник',
+    4 => 'Разбойник',
+    5 => 'Жрец',
+    6 => 'Рыцарь Смерти',
+    7 => 'Шаман',
+    8 => 'Маг',
+    9 => 'Чернокнижник',
+    11 => 'Друид',
+];
+
+$race_names = [
+    1 => 'Человек',
+    2 => 'Орк',
+    3 => 'Дворф',
+    4 => 'Ночной Эльф',
+    5 => 'Нежить',
+    6 => 'Таурен',
+    7 => 'Гном',
+    8 => 'Троль',
+    10 => 'Эльф Крови',
+    11 => 'Дреней',
+];
+?>
