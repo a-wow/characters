15d11f8d9df7de8434d3d27c7eb98c0e3bc73719
Null <107396951+a-wow@users.noreply.github.com>
2024-10-26 13:04:07 +0300

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'search_characters' => 'Character Search',
+    'enter_character_name' => 'Enter character name',
+    'no_details_found' => 'No details found.',
+    'characters_not_found' => 'Characters not found.',
+    'load_details' => 'Loading...',
+    'error_loading_details' => 'Error loading details.',
+    'money' => 'Money: ',
+    'honor_points' => 'Total honor points: ',
+    'arena_points' => 'Arena points: ',
+    'total_kills' => 'Total kills: ',
+    'status' => 'Status: ',
+    'online' => 'Online',
+    'offline' => 'Offline',
+    'level' => 'Level',
+];
