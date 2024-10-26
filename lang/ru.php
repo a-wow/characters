13690f91605526adcc260f3b4fd6e191ea1909ad
Null <107396951+a-wow@users.noreply.github.com>
2024-10-26 13:04:47 +0300

    Create ru.php

---
+++ lang/ru.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'search_characters' => 'Поиск персонажей',
+    'enter_character_name' => 'Введите имя персонажа',
+    'no_details_found' => 'Детали не найдены.',
+    'characters_not_found' => 'Персонажи не найдены.',
+    'load_details' => 'Загрузка...',
+    'error_loading_details' => 'Ошибка загрузки деталей.',
+    'money' => 'Деньги: ',
+    'honor_points' => 'Общее количество чести: ',
+    'arena_points' => 'Очки арены: ',
+    'total_kills' => 'Всего убийств: ',
+    'status' => 'Статус: ',
+    'online' => 'В сети',
+    'offline' => 'Не в сети',
+    'level' => 'Уровень',
+];
